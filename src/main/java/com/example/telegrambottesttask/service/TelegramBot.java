d0e7528bb7c80976a633fd0432f2290798470193
MrVladimir <vladimir.volkov-2013@yandex.ru>
2023-10-21 14:44:56 +0300

    The bot has reactions on the "start" command

---
+++ src/main/java/com/example/telegrambottesttask/service/TelegramBot.java
@@ -0,0 +1,52 @@
+package com.example.telegrambottesttask.service;
+
+import com.example.telegrambottesttask.config.BotConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Slf4j
+@Service
+public class TelegramBot extends TelegramLongPollingBot {
+
+    private final BotConfig CONFIG;
+
+    public TelegramBot(BotConfig CONFIG) {
+        this.CONFIG = CONFIG;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return CONFIG.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return CONFIG.getToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        Long chatId = update.getMessage().getChatId();
+        String text = update.getMessage().getText();
+        prepareAndSendMessage(chatId, text);
+    }
+
+    private void prepareAndSendMessage(long chatId, String textToSend) {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+        executeMessage(message);
+    }
+
+    private void executeMessage(SendMessage message) {
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            log.error(e.getMessage());
+        }
+    }
+}
